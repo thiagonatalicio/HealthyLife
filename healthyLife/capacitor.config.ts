ca70c0e4d8edced6f8313811c02ec72da1af7bb0
thiagonatalicio <thiagonatalicio33@gmail.com>
2026-05-17 19:15:34 -0300

    Mudança do projeto para NgModule
    Apaguei o projeto Standalone e criei um NgModule.

--- healthyLife/capacitor.config.ts
+++ healthyLife/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'healthyLife',
+  appName: 'HealthyLife',
   webDir: 'www'
 };
 
